2486f072351ad0b41f7fb7c0d631139a599ca912
g.demarco <devnull@localhost>
2021-08-16 11:56:40 +0200

    niente di che

--- repository-wrapper/src/test/java/TestClass.java
+++ repository-wrapper/src/test/java/TestClass.java
@@ -4,16 +4,25 @@
  * and open the template in the editor.
  */
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoException;
+import com.mongodb.QueryBuilder;
+import com.mongodb.gridfs.GridFSDBFile;
 import it.bologna.ausl.minio.manager.MinIOWrapper;
+import it.bologna.ausl.minio.manager.MinIOWrapperFileInfo;
 import it.bologna.ausl.minio.manager.exceptions.MinIOWrapperException;
 import it.bologna.ausl.mongowrapper.MongoWrapper;
 import it.bologna.ausl.mongowrapper.exceptions.MongoWrapperException;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.UnknownHostException;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
@@ -27,11 +36,11 @@ public class TestClass {
     public static void main(String[] args) throws UnknownHostException, MongoException, MongoWrapperException, IOException, MinIOWrapperException {
 
         String	minIODBDriver= "org.postgresql.Driver";
-	String minIODBUrl = "jdbc:postgresql://gdml.internal.ausl.bologna.it:5432/minirepo?stringtype=unspecified";
-//	String minIODBUrl = "jdbc:postgresql://babel-big-auslbo.avec.emr.it:5432/minirepo?stringtype=unspecified";
+//	String minIODBUrl = "jdbc:postgresql://gdml.internal.ausl.bologna.it:5432/minirepo?stringtype=unspecified";
+	String minIODBUrl = "jdbc:postgresql://babel-big-auslbo.avec.emr.it:5432/minirepo?stringtype=unspecified";
 	String minIODBUsername= "minirepo";
-	String minIODBPassword ="siamofreschi";
-//	String minIODBPassword ="4fHkXbSEA6D3";
+//	String minIODBPassword ="siamofreschi";
+	String minIODBPassword ="4fHkXbSEA6D3";
         
         String path = "/Procton/Documenti/2020-65400";
 //        String path = "/Relate";
@@ -45,13 +54,28 @@ public class TestClass {
 //        MongoWrapper m = new MongoWrapper("mongodb://argo908:mPw8DApKLaPR@babel908mongo1.avec.emr.it,babel908mongo2.avec.emr.it/doc908?safe=true&replicaSet=avec0");
 //        MongoWrapper m = new MongoWrapper("mongodb://argo908:mPw8DApKLaPR@babel908mongo1.avec.emr.it/doc908?safe=true");
         //  MongoWrapper m = new MongoWrapper("mongodb://argo909:BIKJBnwosLs7@babel909mongo.avec.emr.it/doc909?safe=true");
-          String mongoUri = "mongodb://argo:siamofreschi@babelmongotest01-auslbo.avec.emr.it/doc?safe=true"; // gdml
+//          String mongoUri = "mongodb://argo:siamofreschi@babelmongotest01-auslbo.avec.emr.it/doc?safe=true"; // gdml
 //          String mongoUri = "mongodb://argo960:Fj0pdiENBdNU@babel960mongo1.avec.emr.it,babel960mongo2.avec.emr.it/doc960?safe=true&replicaSet=avec0";
 //          String mongoUri = "mongodb://argo106:Ushaez4ajei2@babel106mongo1.avec.emr.it,babel106mongo2.avec.emr.it/doc106?safe=true&replicaSet=avec0";
 //          String mongoUri = "mongodb://argo102:e37jTcIeTp0w@babel102mongo.avec.emr.it/doc102?safe=true";
-           MongoWrapper m = MongoWrapper.getWrapper(true, mongoUri, minIODBDriver, minIODBUrl, minIODBUsername, minIODBPassword, "105", null);
-        String res = m.put(new File("Email_disdetta vaccino anticovid_11836971.eml"), "Email_disdetta vaccino anticovid_11836971.eml", "/gdm/test", true);
-        System.out.println("res: " + res);
+//           MongoWrapper m = MongoWrapper.getWrapper(true, mongoUri, minIODBDriver, minIODBUrl, minIODBUsername, minIODBPassword, "105", null);
+//          IOUtils.copy(m.get("5825d792e4b0527a032cbbbc"), new FileOutputStream("aaaaa.eml"));
+//        System.out.println(m.getFileName("5a68b996e4b0824256b7ef93"));
+        MinIOWrapper m = new MinIOWrapper(minIODBDriver, minIODBUrl, minIODBUsername, minIODBPassword);
+        Arrays.asList("5e1a6f5fd5de52bfe69b56f4", "5e1a6e08d5de52bfe69b56ee", "5e1a6944d5de52bfe69b56c4", "5e17115ad5de52bfe69a9f7b", "5e1a6fc2d5de52bfe69b56fa")
+                .stream().forEach(s -> {
+                    MinIOWrapperFileInfo fileInfoByUuid = m.getFileInfoByUuid(s);
+                    System.out.println(fileInfoByUuid.getFileId());
+//                    m.deleteByFileUuid(s);
+//                    m.removeByFileId(fileInfoByUuid.getFileId(), false);
+        });
+//        String res = m.put(new File("288660_24 Jan 2018 10 45 10 GMT medicina.legale@pec.aosp.bo.it.eml"), "5a68b996e4b0824256b7ef93", "288660_24 Jan 2018 10 45 10 GMT medicina.legale@pec.aosp.bo.it.eml", "/TRASH_DIR/Argo/PECGW_STORE/medicina.legale@pec.aosp.bo.it/SENT/288660_24 Jan 2018 10 45 10 GMT medicina.legale@pec.aosp.bo.it.eml/288660_24 Jan 2018 10 45 10 GMT medicina.legale@pec.aosp.bo.it.eml", true);
+//        System.out.println("res: " + res);
+//        m.rename("5e1a6f5fd5de52bfe69b56f4", "4148657_12 Jan 2020 01 20 26 GMT UTF-servizi-fattutrazione@enom.com_2.eml");
+//        m.rename("5e1a6e08d5de52bfe69b56ee", "4148657_12 Jan 2020 01 20 26 GMT UTF-servizi-fattutrazione@enom.com_1.eml");
+//        m.rename("5e1a6944d5de52bfe69b56c4", "4148657_12 Jan 2020 01 20 26 GMT UTF-servizi-fattutrazione@enom.com.eml");
+//        m.rename("5e17115ad5de52bfe69a9f7b", "4142745_09 Jan 2020 12 36 38 GMT UTF-nnnn@enom.com.eml");
+//        m.rename("5e1a6fc2d5de52bfe69b56fa", "4148657_12 Jan 2020 01 20 26 GMT UTF-servizi-fattutrazione@enom.com_3.eml");
         System.exit(0);
 //       MongoWrapper m = new MongoWrapper("mongodb://argo:siamocaldi@babelmongo1,babelmongo2/prod?safe=true&replicaSet=prod0");
 //        m.getDirFiles(path).stream().forEach(uuid -> {System.out.println(uuid + ": " + m.getFileName(uuid));});
