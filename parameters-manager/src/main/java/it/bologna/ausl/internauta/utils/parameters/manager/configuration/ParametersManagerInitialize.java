d1d6dfc2df624fb4c5e703aaa1b1c6d527142a9d
g.demarco <devnull@localhost>
2022-02-28 18:11:31 +0100

    mancavano dei files

---
+++ parameters-manager/src/main/java/it/bologna/ausl/internauta/utils/parameters/manager/configuration/ParametersManagerInitialize.java
@@ -0,0 +1,30 @@
+package it.bologna.ausl.internauta.utils.parameters.manager.configuration;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Definisce il Bean per avere mappa codice-azienda->id-azienda
+ * @author gdm
+ */
+@Configuration
+public class ParametersManagerInitialize {
+    
+    // Questa sarà in realtà l'istanza del'oggetto concreto implementato dall'applicazione utilizzatrice (es. internauta-service)
+    @Autowired
+    private ParametersManagerConfiguration parametersManagerConfiguration;
+    
+    /**
+     * Questo Bean permette di poter avere la mappa codice-azienda->id-azienda inserendo la proprieta di classe:
+     * @Autowired
+     * @Qualifier("codiceAziendaIdAziendaMap")
+     * private Map<String, Integer> codiceAziendaIdAziendaMap;
+     * @return 
+     */
+    @Bean(name = "codiceAziendaIdAziendaMap")
+    public Map<String, Integer> codiceAziendaIdAziendaMap() {
+        return this.parametersManagerConfiguration.getCodiceAziendaIdAziendaMap();
+    }
+}
