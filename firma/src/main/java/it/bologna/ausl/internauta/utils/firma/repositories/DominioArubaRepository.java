f428829114c3eafea965ee243608356a7ff3e13b
g.demarco <giuseppe.demarco@nextsw.it>
2022-05-06 12:44:31 +0200

    firma aruba: reperimento hostId dal dominio

--- firma/src/main/java/it/bologna/ausl/internauta/utils/firma/repositories/DominioArubaRepository.java
+++ firma/src/main/java/it/bologna/ausl/internauta/utils/firma/repositories/DominioArubaRepository.java
@@ -2,6 +2,7 @@ package it.bologna.ausl.internauta.utils.firma.repositories;
 
 import it.bologna.ausl.model.entities.firma.DominioAruba;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -12,4 +13,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "dominioaruba", path = "dominioaruba", exported = false)
 public interface DominioArubaRepository extends QuerydslPredicateExecutor<DominioAruba>, JpaRepository<DominioAruba, String> {
+    
+    @Query(value = "select host_id from firma.configurations c where to_jsonb(cast(:dominio as text)) <@ (params -> 'ArubaSignService' -> 'SupportedDomini')", nativeQuery = true)
+    public String getHostId(String dominio);
 }
