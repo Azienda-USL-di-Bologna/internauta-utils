221e38dca18e77351993729cd6add28a1a089c4f
g.demarco <giuseppe.demarco@nextsw.it>
2022-07-07 14:20:30 +0200

    check version firma jnj

--- firma/src/main/java/it/bologna/ausl/internauta/utils/firma/jnj/controllers/FirmaJnJRestController.java
+++ firma/src/main/java/it/bologna/ausl/internauta/utils/firma/jnj/controllers/FirmaJnJRestController.java
@@ -1,5 +1,6 @@
 package it.bologna.ausl.internauta.utils.firma.jnj.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.bologna.ausl.internauta.utils.firma.data.exceptions.SignParamsException;
 import it.bologna.ausl.internauta.utils.firma.data.jnj.SignParams;
@@ -10,12 +11,22 @@ import it.bologna.ausl.internauta.utils.firma.remota.exceptions.http.ControllerH
 import it.bologna.ausl.internauta.utils.firma.repositories.RequestParameterRepository;
 import it.bologna.ausl.internauta.utils.firma.utils.CommonUtils;
 import it.bologna.ausl.model.entities.firma.RequestParameter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.ZonedDateTime;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +46,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "${firma.jnj.mapping.url}")
 public class FirmaJnJRestController implements ControllerHandledExceptions {
 
+    private static final Logger log = LoggerFactory.getLogger(FirmaJnJRestController.class);
+    
     @Autowired
     private RequestParameterRepository requestParameterRepository;
     
@@ -46,6 +59,9 @@ public class FirmaJnJRestController implements ControllerHandledExceptions {
     
     @Value("${firma.jnj.params-expire-seconds}")
     private Integer firmaJnJParamsExpireSeconds;
+    
+    @Value("${firma.jnj.client-info-file}")
+    private String clientInfoFilePath;
 
     @RequestMapping(value = "/test/{nome}/{cognome}", method = RequestMethod.GET)
     public String test(@PathVariable String nome, @PathVariable String cognome) {
@@ -97,6 +113,30 @@ public class FirmaJnJRestController implements ControllerHandledExceptions {
         requestParameter.setExpireOn(ZonedDateTime.now().plusSeconds(firmaJnJParamsExpireSeconds));
         RequestParameter savedRequestParamter = requestParameterRepository.save(requestParameter);
         return token;
+    }
+    
+    @RequestMapping(value = "/checkUpdate", method = RequestMethod.GET, produces = "application/octet-stream")
+    public void checkUpdate(@RequestParam(required = true) String version, HttpServletResponse response) throws IOException {
+        File clientInfoFile = new File(this.clientInfoFilePath);
+        if (clientInfoFile.exists()) {
+            Map<String, Object> clientInfo = objectMapper.readValue(clientInfoFile, new TypeReference<Map<String, Object>>(){});
+            String actualVersion = (String) clientInfo.get("version");
+            String msiInstallerFileName = (String) clientInfo.get("msi-file");
+            DefaultArtifactVersion versionObject = new DefaultArtifactVersion(version);
+            DefaultArtifactVersion actualVersionObject = new DefaultArtifactVersion(actualVersion);
+            if (actualVersionObject.compareTo(versionObject) > 0) {
+                response.setStatus(HttpStatus.CREATED.value());
+                log.info(String.format("new version detected: client: %s server %s", version, actualVersion));
+                ServletOutputStream outputStream = response.getOutputStream();
+                File msiInstallerFile = new File(msiInstallerFileName);
+                Files.copy(msiInstallerFile.toPath(), outputStream);
+            } else {
+                response.setStatus(HttpStatus.CREATED.value());
+            }
+        } else {
+            log.warn(String.format("client info file not found in path: %s", clientInfoFile.getAbsolutePath()));
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
     }
     
     private String getFirmaJnJServerUrl(HttpServletRequest request) {
