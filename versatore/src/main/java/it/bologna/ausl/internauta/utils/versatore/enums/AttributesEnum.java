7276e3296dea042ebc77a2d812c8329715f0ee11
Giuseppe Russo <g.russo@nsi.it>
2022-11-14 18:53:45 +0100

    Mapping degli attributi

---
+++ versatore/src/main/java/it/bologna/ausl/internauta/utils/versatore/enums/AttributesEnum.java
@@ -0,0 +1,358 @@
+package it.bologna.ausl.internauta.utils.versatore.enums;
+
+/**
+ * Enum per mappare tutti i metadati dell'oggetto da versare ad Infocert.
+ * 
+ * @author Giuseppe Russo <g.russo@nsi.it>
+ */
+public enum AttributesEnum {
+    /**
+     * gg-mm-aa<br>
+     * Metadato mantenuto indipendentemente dalle Linee Guida.<br>
+     * <b>Required</b>
+     */
+    DATA_DOCUMENTO("data_documento_dt"),
+    
+    /**
+     * documentID o identificativo SdI per le fatture o ID SAP o ID del sistema 
+     * di gestione documentale o anche nome file.<br>
+     * <b>Required</b>
+     */
+    IDENTIFICATIVO_DOCUMENTO("iddocid_s"),
+    
+    /**
+     * Hash del documento calcolato dal produttore del PdV.<br>
+     * <b>Required</b>
+     */
+    IMPRONTA("iddocimp_s"),
+    
+    /**
+     * Algoritmo applicato, default: SHA-256.<br>
+     * <b>Required</b>
+     */
+    ALGORITMO("iddocalg_s"),
+    
+    /**
+     * Valori ammessi:<br>
+     *  a) creazione tramite l’utilizzo di strumenti software,<br>
+     *  b) acquisizione per via telematica o della copia per immagine,<br>
+     *  c) transazioni o processi informatici o moduli o formulari resi disponibili all’utente<br>
+     *  d) generazione da registrazioni o banca dati<br>
+     *  Può essere un default.<br>
+     *  Si può indicare proprio solo la lettera. es. ‘a’ per fattura elettronica o ‘b’ per fattura cartacea scansionata.<br>
+     *  Per Babel il valore di default è 'b'.<br>
+     * <b>Required</b>
+     */
+    MODALITA_DI_FORMAZIONE("modform_s"),
+    
+    /**
+     * Fatture attive, passive, notifiche, libro giornale, registro.<br>
+     * Può essere un default.<br>
+     * <b>Required</b>
+     */
+    TIPOLOGIA_DOCUMENTALE("tipdoc_s"),
+    
+    /**
+     * In uscita o in entrata (per le fatture rispetto al ciclo attivo o passivo), interno (per i libri contabili).<br>
+     * Può essere un default.<br>
+     * Si può indicare proprio solo la lettera U, E, I.<br>
+     * <b>Required</b>
+     */
+    TIPOLOGIA_DI_FLUSSO("datregflusso_s"),
+    
+    /**
+     * Valori ammessi:<br>
+     * • Nessuno<br>
+     * • Protocollo Ordinario/ Protocollo Emergenza<br>
+     * • Repertorio/Registro<br>
+     * Può essere un default.<br>
+     * <b>Required</b>
+     */
+    TIPO_REGISTRO("datregtipreg_s"),
+    
+    /**
+     * Data/ora di registrazione del documento. Può comprendere anche l’ora.<br>
+     * <b>Required</b>
+     */
+    DATA_REGISTRAZIONE("datregdata_dt"),
+    
+    /**
+     * Numero di registrazione del documento.<br>
+     * Numero di protocollo, ID SDI, numero documento fornitore, numero concatenato mese/anno.<br>
+     * <b>Required</b>
+     */
+    NUMERO_DOCUMENTO("datregnum_s"),
+    
+    /**
+     * Codice identificativo del registro in cui il documento viene registrato.<br>
+     * Nel caso in cui il tipo registro sia protocollo ordinario/protocollo emergenza, o Repertorio/Registro.<br>
+     * Può essere un default.<br>
+     * <i>Optional</i>
+     */
+    CODICE_REGISTRO("datregid_s"),
+    
+    /**
+     * Testo libero. Es. fattura attiva<br>
+     * <b>Required</b>
+     */
+    OGGETTO("ogg_s"),
+    
+    /**
+     * Valori ammessi:<br>
+     * • assegnatario<br>
+     * • autore<br>
+     * • mittente<br>
+     * • destinatario<br>
+     * • operatore<br>
+     * • produttore<br>
+     * • RGD (Responsabile della Gestione Documentale)<br>
+     * • RSP (Responsabile del Servizio di Protocollo)<br>
+     * • Soggetto che effettua la registrazione<br>
+     * • altro<br>
+     * Almeno un soggetto che effettua la registrazione del documento (tipicamente l’Organizzazione che protocolla) 
+     * e un autore o un mittente va indicato obbligatoriamente.<br>
+     * Può essere ricorsivo.<br>
+     * In questo caso, il set di metadati ripetuto dovrà essere distinto con numeri progressivi.<br>
+     * es. &lt;field label="Soggetti Ruolo 2 " name="soggru_2_s">Destinatario&gt;<br>
+     * <b>Required</b>
+     */
+    RUOLO("soggru_s"),
+    
+    /**
+     * Valori ammessi:<br>
+     * • PF per persona fisica<br>
+     * • PG per organizzazione<br>
+     * • PAI per amministrazione pubblica italiana<br>
+     * • PAE per le Amministrazioni Pubbliche estere<br>
+     * • SW per i documenti prodotti automaticamente (Se Ruolo = Produttore)<br>
+     * Può essere un default.<br>
+     */
+    TIPO_SOGGETTO("soggtip_s"),
+    
+    /**
+     * Obbligatorio se Tipo soggetto: PF<br>
+     * <i>Optional</i>
+     */
+    COGNOME("cognome_s"),
+    
+    /**
+     * Obbligatorio se Tipo soggetto: PF<br>
+     * <i>Optional</i>
+     */
+    NOME("nome_s"),
+    
+    /**
+     * Obbligatorio se Tipo soggetto: PG, PAI, PAE, AS, SW.<br>
+     * Per le PA può essere ricorsivo e riferito al Codice IPA dell’Amministrazione, dell’AOO, dell’UOR, o all’Ufficio.<br>
+     * <i>Optional</i>
+     */
+    DEMONIMAZIONE("denominazione_s"),
+    
+    /**
+     * O Partita Iva.<br>
+     * <i>Optional</i>
+     */
+    CODICE_FISCALE("codicefiscale_s"),
+    
+    /**
+     * Se presenti.<br>
+     * <i>Optional</i>
+     */
+    INDIRIZZI_DIGITALI_DI_RIFERIMENTO("indirizzidigit_s"),
+    
+    /**
+     * Valori ammessi: 0, 1, 2 …<br>
+     * Es. allegato a un contratto, considerato come un secondo file, versato separatamente, 
+     * con un suo set di metadati e collegato al ‘contratto padre’ mediante questo metadato.<br>
+     * Non rientrano gli allegati ‘embedded’ nei documenti.<br>
+     * <b>Required</b>
+     */
+    ALLEGATI_NUMERO("alleg_i"),
+    
+    /**
+     * Da indicare per ogni allegato se Numero allegati > 0<br>
+     * <i>Optional</i>
+     */
+    ID_DOC_INDICE_ALLEGATI("allegiddoc_s"),
+    
+    /**
+     * Testo libero.<br>
+     * <i>Optional</i>
+     */
+    DESCRIZIONE_ALLEGATI("allegdesc_s"),
+    
+    /**
+     * Codifica del documento secondo il Piano di classificazione utilizzato (obbligatorio per le PA).<br>
+     * <i>Optional</i>
+     */
+    INDICE_DI_CLASSIFICAZIONE("indclass_s"),
+    
+    /**
+     * Descrizione per esteso dell’Indice di classificazione indicato (obbligatorio per le PA).<br>
+     * <i>Optional</i>
+     */
+    DESCRIZIONE_CLASSIFICAZIONE("descrclass_s"),
+    
+    /**
+     * URI del Piano di classificazione pubblicato.<br>
+     * <i>Optional</i>
+     */
+    PIANO_CLASSIFICAZIONE("pianoclass_s"),
+    
+    /**
+     * Boolean true/false in relazione alla visibilità nei Sistemi di Gestione Documentale di formazione e gestione.<br>
+     * (Non comporta cifrature automatiche, che sono da richiedere nella prima pagina della Scheda).<br>
+     * <b>Required</b>
+     */
+    RISERVATO("riservato_b"),
+    
+    /**
+     * Mimetype es. Application/XML<br>
+     * vedi Allegato 2 Linee Guida.<br>
+     * <b>Required</b>
+     */
+    IDENTIFICATIVO_DEL_FORMATO("formid_s"),
+    
+    /**
+     * Se disponibile e da validare soprattutto se si conservano formati proprietari.<br>
+     * vedi Allegato 2 Linee Guida.<br>
+     * <i>Optional</i>
+     */
+    PRODOTTO_SOFTWARE_NOME("formnom_s"),
+    
+    /**
+     * Se disponibile e da validare soprattutto se si conservano formati proprietari.<br>
+     * vedi Allegato 2 Linee Guida.<br>
+     * <i>Optional</i>
+     */
+    PRODOTTO_SOFTWARE_VERSIONE("formvers_s"),
+    
+    /**
+     * Se disponibile e da validare soprattutto se si conservano formati proprietari.<br>
+     * vedi Allegato 2 Linee Guida.<br>
+     * <i>Optional</i>
+     */
+    PRODOTTO_SOFTWARE_PRODUTTORE("formprod_s"),
+    
+    /**
+     * Verifica fatta dal produttore del PdV sulla presenza/assenza.<br>
+     * Obbligatorio nel solo caso di modalità di formazione doc = a/b.<br>
+     * boolean true/false<br>
+     * <i>Optional</i>
+     */
+    VERIFICA_FIRMA_DIGITALE("firm_b"),
+    
+    /**
+     * Verifica fatta dal produttore del PdV sulla presenza/assenza.<br>
+     * Obbligatorio nel solo caso di modalità di formazione doc = a/b.<br>
+     * boolean true/false<br>
+     * <i>Optional</i>
+     */
+    VERIFICA_MARCA_TEMPORALE("marc_b"),
+    
+    /**
+     * Verifica fatta dal produttore del PdV sulla presenza/assenza.<br>
+     * Obbligatorio nel solo caso di modalità di formazione doc = a/b.<br>
+     * boolean true/false<br>
+     * <i>Optional</i>
+     */
+    VERIFICA_SIGILLO("sig_b"),
+    
+    /**
+     * Verifica fatta dal produttore del PdV sulla presenza/assenza.<br>
+     * Obbligatorio nel solo caso di modalità di formazione doc = a/b.<br>
+     * boolean true/false<br>
+     * <i>Optional</i>
+     */
+    VERIFICA_CONFORMITA_COPIE("cop_b"),
+    
+    /**
+     * Identificativo del fascicolo o della serie.<br>
+     * es. 2.3.1.2020 indica il primo fascicolo dell’anno 2020 nel titolo 2 classe 3 del titolario di classificazione.<br>
+     * <i>Optional</i>
+     */
+    ID_AGGREGAZIONE("idagg_s"),
+    
+    /**
+     * Identificativo univoco e persistente del documento principale, da popolare nel versamento 
+     * di un documento allegato al documento principale, per creare un vincolo tra i due.<br>
+     * <i>Optional</i>
+     */
+    IDENTIFICATIVO_DOCUMENTO_PRINCIPALE("iddocprinc_s"),
+    
+    /**
+     * Nome alfanumerico del documento/file così come riconosciuto all’esterno.<br>
+     * <b>Required</b>
+     */
+    NOME_FILE("nomefile_s"),
+    
+    /**
+     * 1, 2, 3, può essere per es. un default: 1.<br>
+     * <b>Required</b>
+     */
+    VERSIONE_DEL_DOCUMENTO("vers_i"),
+    
+    /**
+     * Nel caso di versione > 1 (rettifica)<br>
+     * Valori ammessi:<br>
+     * • annullamento<br>
+     * • rettifica<br>
+     * • integrazione<br>
+     * • annotazione<br>
+     * Metadato volto a tracciare la presenza di operazioni di modifica effettuate sul documento.<br>
+     * <i>Optional</i>
+     */
+    TRACCIATURA_MODIFICHE_TIPO("modiftipo_s"),
+    
+    /**
+     * Nel caso di versione > 1 (rettifica)<br>
+     * Come da ruolo = Operatore definito nel metadato Soggetti.<br>
+     * <i>Optional</i>
+     */
+    SOGGETTO_AUTORE_DELLA_MODIFICA("modifaut_s"),
+    
+    /**
+     * Nel caso di versione > 1 (rettifica)<br>
+     * Data e ora della modifica.<br>
+     * <i>Optional</i>
+     */
+    TRACCIATURA_MODIFICHE_DATA("modifdata_dt"),
+    
+    /**
+     * Nel caso di versione > 1 (rettifica)<br>
+     * Identificativo versione precedente.<br>
+     * <i>Optional</i>
+     */
+    TRACCIATURA_MODIFICHE_ID_DOC_VERSIONE_PRECEDENTE("modifidprec_s"),
+    
+    /**
+     * Es. 10 anni<br>
+     * (non comporta scarti automatici).<br>
+     * <i>Optional</i>
+     */
+    TEMPO_DI_CONSERVAZIONE("tempcons_s"),
+    
+    NOTE("note_s"),
+    
+    /**
+     * Segnatura del protocollo.<br>
+     * <b>Required<b> Per la sola classe DOCUMENTO PROTOCOLLATO. 
+     * In tal caso vanno aggiunti obbligatoriamente anche i metadati relativi alla classificazione.
+     */
+    SEGNATURA("segnatura_s");
+
+    private final String attributo;       
+
+    private AttributesEnum(String s) {
+        attributo = s;
+    }
+
+    public boolean equalsName(String otherName) {
+        // (otherName == null) check is not needed because name.equals(null) returns false 
+        return attributo.equals(otherName);
+    }
+
+    public String toString() {
+       return this.attributo;
+    }
+}
--- versatore/src/main/java/it/bologna/ausl/internauta/utils/versatore/services/InfocertVersatoreService.java
+++ versatore/src/main/java/it/bologna/ausl/internauta/utils/versatore/services/InfocertVersatoreService.java
@@ -2,10 +2,21 @@ package it.bologna.ausl.internauta.utils.versatore.services;
 
 import it.bologna.ausl.internauta.utils.versatore.VersamentoInformation;
 import it.bologna.ausl.internauta.utils.versatore.VersatoreDocs;
+import it.bologna.ausl.internauta.utils.versatore.enums.AttributesEnum;
 import it.bologna.ausl.internauta.utils.versatore.utils.VersatoreConfigParams;
 import it.bologna.ausl.model.entities.scripta.Doc;
 import it.bologna.ausl.model.entities.versatore.Configuration;
+import it.bologna.ausl.utils.versatore.infocert.wsclient.DocumentAttribute;
+import it.bologna.ausl.utils.versatore.infocert.wsclient.GenericDocument;
+import it.bologna.ausl.utils.versatore.infocert.wsclient.GenericDocumentService;
+import java.net.MalformedURLException; 
+import java.net.URL; 
+import java.util.ArrayList; 
+import java.util.List; 
 import java.util.Map;
+import javax.activation.DataHandler; 
+import javax.activation.FileDataSource; 
+import javax.xml.ws.BindingProvider; 
 import javax.persistence.EntityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,31 +26,62 @@ import org.slf4j.LoggerFactory;
  * @author Giuseppe Russo <g.russo@nsi.it>
  */
 public class InfocertVersatoreService extends VersatoreDocs {
-    
+
     private static final Logger log = LoggerFactory.getLogger(InfocertVersatoreService.class);
-    private static final String INFOCERT_VERSATORE_SERVICE = "InfocertVersatoreService";  
-    
+    private static final String INFOCERT_VERSATORE_SERVICE = "InfocertVersatoreService";
+
     private final String infocertVersatoreServiceEndPointUri;
 
     public InfocertVersatoreService(EntityManager entityManager, VersatoreConfigParams versatoreConfigParams, Configuration configuration) {
         super(entityManager, versatoreConfigParams, configuration);
-        
+
         Map<String, Object> firmaRemotaConfiguration = configuration.getParams();
         Map<String, Object> infocertServiceConfiguration = (Map<String, Object>) firmaRemotaConfiguration.get(INFOCERT_VERSATORE_SERVICE);
         infocertVersatoreServiceEndPointUri = infocertServiceConfiguration.get("InfocertVersatoreServiceEndPointUri").toString();
-        log.info("URI: %s", infocertVersatoreServiceEndPointUri);
+        log.info(String.format("URI: %s", infocertVersatoreServiceEndPointUri));
     }
 
     @Override
     public VersamentoInformation versa(VersamentoInformation versamentoInformation) {
-        
+
         Integer idDoc = versamentoInformation.getIdDoc();
-        
+
         Doc doc = entityManager.find(Doc.class, idDoc);
-        
+
         log.info(doc.getOggetto());
-        
+
+        log.info(AttributesEnum.RISERVATO.toString());
+
+        GenericDocumentService iss;
+
+        try {
+            iss = new GenericDocumentService(new URL(infocertVersatoreServiceEndPointUri));
+
+            GenericDocument is = iss.getGenericDocumentPort();
+            BindingProvider bp = (BindingProvider) is;
+            bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                    infocertVersatoreServiceEndPointUri);
+            List<DocumentAttribute> attributes = new ArrayList();
+
+            DocumentAttribute attr1 = new DocumentAttribute();
+            attr1.setName(AttributesEnum.NOME.toString());
+            attr1.setValue("Mario");
+            DocumentAttribute attr2 = new DocumentAttribute();
+            attr2.setName(AttributesEnum.COGNOME.toString());
+            attr2.setValue("Rossi");
+            String docID = "0123456";
+
+            attributes.add(attr1);
+            attributes.add(attr2);
+//            DataHandler data = new DataHandler(new FileDataSource(arg[1]));
+            DataHandler data = null;
+            is.submitDocument(docID, attributes, data);
+
+        } catch (MalformedURLException e) {
+            log.error(e.getMessage());
+        }
+
         return versamentoInformation;
     }
-    
+
 }
