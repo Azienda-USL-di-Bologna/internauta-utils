5bcf7f21c4bb52a43c4c30182f95e5a02bf18631
gusgus <f.gusella@nsi.it>
2023-11-29 16:28:24 +0100

    modifiche al masterjobs, colonna errore svuotata al running, inserimento framework per usare masterjobs-api

--- masterjobs/src/main/java/it/bologna/ausl/internauta/utils/masterjobs/repository/JobReporitory.java
+++ masterjobs/src/main/java/it/bologna/ausl/internauta/utils/masterjobs/repository/JobReporitory.java
@@ -1,21 +1,24 @@
 package it.bologna.ausl.internauta.utils.masterjobs.repository;
 
-import it.bologna.ausl.internauta.utils.masterjobs.workers.jobs.JobWorkerData;
 import it.bologna.ausl.model.entities.masterjobs.Job;
-import java.util.UUID;
+import it.bologna.ausl.model.entities.masterjobs.QJob;
+import it.bologna.ausl.model.entities.masterjobs.projections.generated.JobWithPlainFields;
+import it.nextsw.common.data.annotations.NextSdrRepository;
+import it.nextsw.common.repositories.NextSdrQueryDslRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  *
- * @author Top
+ * @author gusgus
  */
-@RepositoryRestResource(collectionResourceRel = "job", path = "job", exported = false)
-public interface JobReporitory extends QuerydslPredicateExecutor<Job>, JpaRepository<Job, Long> {
+@NextSdrRepository(repositoryPath = "${masterjobs.mapping.url.root}/job", defaultProjection = JobWithPlainFields.class)
+@RepositoryRestResource(collectionResourceRel = "job", path = "job", exported = false, excerptProjection = JobWithPlainFields.class)
+public interface JobReporitory extends
+        NextSdrQueryDslRepository<Job, Long, QJob>,
+        JpaRepository<Job, Long>  {
     
     @Query(value = "select masterjobs.calcola_md5(?1,cast(?2 as jsonb),?3)", nativeQuery = true)
     public String calcolaMD5(
