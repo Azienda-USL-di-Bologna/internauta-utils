276d0af3178cbb370fbb3fe6b4b4d92058a83323
Giuseppe Russo <g.russo@dilaxia.com>
2024-03-19 11:48:10 +0100

    Fix attributo soggru_s facendolo partire da 1

--- versatore/src/main/java/it/bologna/ausl/internauta/utils/versatore/plugins/infocert/InfocertVersatoreService.java
+++ versatore/src/main/java/it/bologna/ausl/internauta/utils/versatore/plugins/infocert/InfocertVersatoreService.java
@@ -366,11 +366,11 @@ public class InfocertVersatoreService extends VersatoreDocs {
                 .addNewAttribute(docAttributes, InfocertAttributesEnum.OGGETTO, docDetail.getOggetto());
         
         // Metadati degli Agenti (Soggetti)
-        addNewAttribute(docAttributes, InfocertAttributesEnum.RUOLO, "produttore")
-                .addNewAttribute(docAttributes, InfocertAttributesEnum.TIPO_SOGGETTO, "SW")
-                .addNewAttribute(docAttributes, InfocertAttributesEnum.DENOMINAZIONE, docDetail.getIdAzienda().getDescrizione());
+        int index = 1;  // Indice per i metadati ricorsivi del ruolo, il numero 1 è default ed è il produttore
+        addNewAttribute(docAttributes, InfocertAttributesEnum.RUOLO_N, "produttore")
+                .addNewAttribute(docAttributes, InfocertAttributesEnum.TIPO_SOGGETTO_N, "SW")
+                .addNewAttribute(docAttributes, InfocertAttributesEnum.DENOMINAZIONE_N, docDetail.getIdAzienda().getDescrizione());
         
-        int index = 2;  // Indice per i metadati ricorsivi del ruolo, il numero 1 è default ed è il produttore
         addNewAttribute(docAttributes, InfocertAttributesEnum.RUOLO_N, index, "redattore")
                 .addNewAttribute(docAttributes, InfocertAttributesEnum.TIPO_SOGGETTO_N, index, "PF")
                 .addNewAttribute(docAttributes, InfocertAttributesEnum.COGNOME_N, index, docDetail.getIdPersonaRedattrice().getCognome())
