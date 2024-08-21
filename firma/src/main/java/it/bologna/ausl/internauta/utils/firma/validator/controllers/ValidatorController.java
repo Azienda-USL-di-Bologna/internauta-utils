010d83ad05a12648c9bbe13daedf8d164503cf44
gdm <giuseppe.demarco@nextsw.it>
2024-08-21 17:09:45 +0200

    verificatore certificato con dss validator

--- firma/src/main/java/it/bologna/ausl/internauta/utils/firma/validator/controllers/ValidatorController.java
+++ firma/src/main/java/it/bologna/ausl/internauta/utils/firma/validator/controllers/ValidatorController.java
@@ -1,10 +1,12 @@
 package it.bologna.ausl.internauta.utils.firma.validator.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import it.bologna.ausl.dss.data.DSSValidatorReponse;
 import it.bologna.ausl.dss.data.exceptions.NoSignException;
 import it.bologna.ausl.internauta.utils.firma.configuration.FirmaHttpClientConfiguration;
+import it.bologna.ausl.internauta.utils.firma.data.jnj.SignParams;
 import it.bologna.ausl.internauta.utils.firma.utils.ConfigParams;
 import it.bologna.ausl.internauta.utils.firma.exceptions.FirmaHttpException;
 import it.bologna.ausl.internauta.utils.firma.repositories.RequestParameterRepository;
@@ -28,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import it.bologna.ausl.internauta.utils.firma.remota.exceptions.http.FirmaRemotaControllerHandledExceptions;
+import it.bologna.ausl.internauta.utils.firma.utils.ConfigParams.ExternalSignAndCertificateValidatorParamsKey;
 import it.bologna.ausl.internauta.utils.firma.validator.exceptions.DssResponseException;
 import it.bologna.ausl.minio.manager.MinIOWrapper;
 import it.bologna.ausl.minio.manager.exceptions.MinIOWrapperException;
@@ -38,6 +41,8 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import org.apache.commons.io.IOUtils;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -64,8 +69,6 @@ public class ValidatorController implements FirmaRemotaControllerHandledExceptio
     
     @Autowired
     private ObjectMapper objectMapper;
-    
-    
 
     @RequestMapping(value = "/test/{nome}/{cognome}", method = RequestMethod.GET)
     public String test(@PathVariable String nome, @PathVariable String cognome) {
@@ -96,7 +99,7 @@ public class ValidatorController implements FirmaRemotaControllerHandledExceptio
         try (InputStream fileIs = StringUtils.hasText(fileRepoFileId)? minIOWrapper.getByFileId(fileRepoFileId): minIOWrapper.getByUuid(fileRepoMongoUuid)) {
             if (fileIs != null) {
                 byte[] file = IOUtils.toByteArray(fileIs);
-                res = callDssValidator(request, validationDate, file);
+                res = validateSignedDocument(request, validationDate, file);
             } else {
                 String error = "il file non è stato trovato nel repository";
                 log.error(error);
@@ -128,7 +131,7 @@ public class ValidatorController implements FirmaRemotaControllerHandledExceptio
         log.info("charset: " + System.getProperty("file.encoding"));
         String res;
         try {
-            res = callDssValidator(request, validationDate, file.getBytes());
+            res = validateSignedDocument(request, validationDate, file.getBytes());
         } catch (DssResponseException ex) {
             return ResponseEntity.internalServerError().body(ex.getMessage());
         } catch (IOException ex) {
@@ -141,13 +144,110 @@ public class ValidatorController implements FirmaRemotaControllerHandledExceptio
         log.info(res);
         return ResponseEntity.ok(res);
     }
+    
+    @RequestMapping(value = "/validateCertificateForFirmaJnj", consumes = "multipart/form-data", method = RequestMethod.POST, produces = "text/plain")
+    public ResponseEntity<String> validateCertificateForFirmaJnj( 
+            @RequestParam("file") MultipartFile file, 
+            //@RequestParam(value = "validationDate", required = false) String validationDate,
+            @RequestParam(value = "validationDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime validationDate,
+            HttpServletRequest request) throws JsonProcessingException, DssResponseException {
+            
+            SignParams.CertificateStatus res = SignParams.CertificateStatus.UNKNOWN;
+            Map<String, String> reportCertificateValidationMap = getReportCertificateValidationMap(file, validationDate, request);
+            if (reportCertificateValidationMap != null && ! reportCertificateValidationMap.isEmpty()) {
+                String indication = reportCertificateValidationMap.get("Indication");
+                String subIndication = reportCertificateValidationMap.get("SubIndication");
+                if (StringUtils.hasText(indication)) {
+                    if (indication.toLowerCase().contains("PASSED".toLowerCase())) {
+                        res = SignParams.CertificateStatus.GOOD;
+                    } else if (indication.toLowerCase().contains("FAILED".toLowerCase())) {
+                        res = SignParams.CertificateStatus.REVOKED;
+                        if (StringUtils.hasText(subIndication)) {
+                            if (subIndication.toLowerCase().contains("EXPIRED".toLowerCase())) {
+                                res = SignParams.CertificateStatus.EXPIRED;
+                            } else if (subIndication.toLowerCase().contains("NOT_YET_VALID".toLowerCase())) {
+                                res = SignParams.CertificateStatus.NOT_YET_VALID;
+                            }
+                        }
+                    } else if (indication.toLowerCase().contains("INDETERMINATE".toLowerCase())) {
+                        res = SignParams.CertificateStatus.UNKNOWN;
+                        if (StringUtils.hasText(subIndication)) {
+                            if (subIndication.toLowerCase().contains("REVOKED".toLowerCase()) && 
+                                    !subIndication.toLowerCase().contains("NOT_REVOKED".toLowerCase())) {
+                                res = SignParams.CertificateStatus.REVOKED;
+                            } else if (subIndication.toLowerCase().contains("EXPIRED".toLowerCase())) {
+                                res = SignParams.CertificateStatus.EXPIRED;
+                            } else if (subIndication.toLowerCase().contains("NOT_YET_VALID".toLowerCase())) {
+                                res = SignParams.CertificateStatus.NOT_YET_VALID;
+                            } else if (subIndication.toLowerCase().contains("OUT_OF_BOUND".toLowerCase())) {
+                                res = SignParams.CertificateStatus.EXPIRED;
+                            }
+                        }
+                    }
+                }
+            }
+        return ResponseEntity.ok(res.toString());
+    }
+    
+     @RequestMapping(value = "/validateCertificate", consumes = "multipart/form-data", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<?> validateCertificate( 
+            @RequestParam("file") MultipartFile file, 
+            @RequestParam(value = "validationDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime validationDate,
+            HttpServletRequest request) throws JsonProcessingException {
+        
+        try {
+            Map<String, String> reportCertificateValidationMap = getReportCertificateValidationMap(file, validationDate, request);
+            return ResponseEntity.ok(reportCertificateValidationMap);
+        } catch (DssResponseException ex) {
+            if (ex.getResponseMap() != null) {
+                return ResponseEntity.internalServerError().body(ex.getResponseMap());
+            } else {
+                ResponseEntity.internalServerError().body(ex.getMessage());
+            }
+        } catch (Throwable ex) {
+            String errorMessage = "errore nella verifica del certificato";
+            log.error(errorMessage, ex);
+            return ResponseEntity.internalServerError().body(ex.getMessage());
+        }
+        return null;
+    }
+    
+    private Map<String, String> getReportCertificateValidationMap(MultipartFile file, LocalDateTime validationDate, HttpServletRequest request) throws DssResponseException {
+        log.info("charset: " + System.getProperty("file.encoding"));
+        Map<String, String> res;
+        try {
+            res = validateCertificate(request, validationDate, file.getBytes());
+        } catch (DssResponseException ex) {
+            res = new HashMap<>();
+            res.put("Errors", ex.getMessage());
+            throw new DssResponseException("errore nella validazione del certificato", res, ex);
+        } catch (IOException ex) {
+            String error = "errore nella chiamata al validatore DSS";
+            log.error(error, ex);
+            res = new HashMap<>();
+            res.put("Errors", error + ": " + ex.getMessage());
+            throw new DssResponseException("errore nella validazione del certificato", res, ex);
+//            return ResponseEntity.internalServerError().body(res);
+        } catch (NoSignException ex) {
+            String error = "errore nella chiamata al validatore DSS, questo errore non dovrebbe mai accadere";
+            log.error(error, ex);
+            res = new HashMap<>();
+            res.put("Errors", error + ": " + ex.getMessage());
+            throw new DssResponseException("errore nella validazione del certificato", res, ex);
+        }
+        try {
+            log.info(this.objectMapper.writeValueAsString(res));
+        } catch (JsonProcessingException ex) {
+        }
+        return res;
+    }
 
-    private String callDssValidator(HttpServletRequest request, LocalDateTime validationDate, byte[] file) throws DssResponseException, IOException, NoSignException {
+    private DSSValidatorReponse callDssValidator(ExternalSignAndCertificateValidatorParamsKey paramKey, HttpServletRequest request, LocalDateTime validationDate, byte[] file) throws DssResponseException, IOException, NoSignException {
         String scheme = request.getScheme();
         String hostname = CommonUtils.getHostname(request);
         Integer port = request.getServerPort();
-//        port = 10008;
-        String externalCheckCertificateUrl = configParams.getExternalCheckCertificateUrl(scheme, hostname, port);
+        
+        String url = configParams.getExternalSignAndCertificateValidator(paramKey, scheme, hostname, port);
         OkHttpClient client = firmaHttpClientConfiguration.getHttpClientManager().getOkHttpClient();
 
         MultipartBody.Builder requestBodyBuilder = new MultipartBody.Builder()
@@ -158,7 +258,7 @@ public class ValidatorController implements FirmaRemotaControllerHandledExceptio
         okhttp3.RequestBody requestBody = requestBodyBuilder.build();
         Response resp = client.newCall(
                 new Request.Builder()
-                    .url(externalCheckCertificateUrl)
+                    .url(url)
                     .post(requestBody).build()).execute();
 
         if (resp.isSuccessful() && resp.body() != null) {
@@ -168,9 +268,8 @@ public class ValidatorController implements FirmaRemotaControllerHandledExceptio
     //            String resString = new String(resp.body().bytes(), Charsets.ISO_8859_1);
                 if (StringUtils.hasText(resString)) {
                     log.info(resString);
-                    DSSValidatorReponse dSSValidatorReponse = new DSSValidatorReponse(resString);
-                    String reportString = dSSValidatorReponse.getReportString();
-                    return reportString;
+                    DSSValidatorReponse dSSValidatorReponse = DSSValidatorReponse.parseFromJson(resString);
+                    return dSSValidatorReponse;
                 } else {
                     String error = "la chiamata al validatore DSS ha tornato una risposta vuota";
                     log.error(error);
@@ -190,5 +289,15 @@ public class ValidatorController implements FirmaRemotaControllerHandledExceptio
             log.error(error);
             throw new DssResponseException(error);
         }
+    }
+    
+    private String validateSignedDocument(HttpServletRequest request, LocalDateTime validationDate, byte[] file) throws DssResponseException, IOException, NoSignException {
+        DSSValidatorReponse dSSValidatorReponse = callDssValidator(ExternalSignAndCertificateValidatorParamsKey.validateDocumentUrl, request, validationDate, file);
+        return dSSValidatorReponse.getSignReportString();
+    }
+    
+    private Map<String, String> validateCertificate(HttpServletRequest request, LocalDateTime validationDate, byte[] file) throws DssResponseException, IOException, NoSignException {
+        DSSValidatorReponse dSSValidatorReponse = callDssValidator(ExternalSignAndCertificateValidatorParamsKey.validateCertificateUrl, request, validationDate, file);
+        return dSSValidatorReponse.getCertificateReportMap();
     }
 }
