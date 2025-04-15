0e08af6a17ee143d5c3e9447a8aae4b857459e8c
t.ravaioli@nextsw.it <t.ravaioli@nextsw.it>
2025-04-15 09:04:59 +0200

    ribalatoneDataConfiguration

---
+++ ribaltone/src/main/java/it/bologna/ausl/internauta/utils/ribaltone/configuration/nextsdr/RibaltoneRestControllerEngineImpl.java
@@ -0,0 +1,13 @@
+package it.bologna.ausl.internauta.utils.masterjobs.configuration.nextsdr;
+
+import it.nextsw.common.controller.RestControllerEngine;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author gsugus
+ */
+@Service
+public class MasterjobsRestControllerEngineImpl extends RestControllerEngine {
+
+}
