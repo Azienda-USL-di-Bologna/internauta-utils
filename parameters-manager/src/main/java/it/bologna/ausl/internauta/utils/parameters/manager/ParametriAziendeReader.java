006572daa5a072d6593f0c91bb35f3d1c427517a
t.ravaioli@nextsw.it <t.ravaioli@nextsw.it>
2025-06-11 14:35:20 +0200

    no message

--- parameters-manager/src/main/java/it/bologna/ausl/internauta/utils/parameters/manager/ParametriAziendeReader.java
+++ parameters-manager/src/main/java/it/bologna/ausl/internauta/utils/parameters/manager/ParametriAziendeReader.java
@@ -55,7 +55,8 @@ public class ParametriAziendeReader {
         ribaltoneConf,
         processaDati,
         usaNuovoRibaltoneVeloce,
-        tabRegistrazioniScriptaActive
+        tabRegistrazioniScriptaActive,
+        abilitaGestioneMassivaArchiviPerResponsabiliAndVicari
     }
     
     @Autowired
