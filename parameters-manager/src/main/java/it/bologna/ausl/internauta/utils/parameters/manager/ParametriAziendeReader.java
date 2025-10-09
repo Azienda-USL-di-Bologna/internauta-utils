512e42631a1f7cf9f83335e13dda35d996f22bd9
gusgus <f.gusella@nsi.it>
2025-10-09 15:29:04 +0200

    fixino al masterjobs

--- parameters-manager/src/main/java/it/bologna/ausl/internauta/utils/parameters/manager/ParametriAziendeReader.java
+++ parameters-manager/src/main/java/it/bologna/ausl/internauta/utils/parameters/manager/ParametriAziendeReader.java
@@ -57,7 +57,8 @@ public class ParametriAziendeReader {
         processaDati,
         usaNuovoRibaltoneVeloce,
         abilitaGestioneMassivaArchiviPerResponsabiliAndVicari,
-        maxSchedePerFiltriEdiEfiWithAutoMode
+        maxSchedePerFiltriEdiEfiWithAutoMode,
+        notificaGestioneMassivaArchiviPerUtentiCoinvolti
     }
 
     @Autowired
