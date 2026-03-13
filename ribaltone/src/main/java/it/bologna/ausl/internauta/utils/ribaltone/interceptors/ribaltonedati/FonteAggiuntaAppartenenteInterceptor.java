65323bd4d6f8344b7e39e18507c4deaad838185d
DOnza Michele <m.donza@nextsw.it>
2026-03-13 15:31:44 +0100

    fix

--- ribaltone/src/main/java/it/bologna/ausl/internauta/utils/ribaltone/interceptors/ribaltonedati/FonteAggiuntaAppartenenteInterceptor.java
+++ ribaltone/src/main/java/it/bologna/ausl/internauta/utils/ribaltone/interceptors/ribaltonedati/FonteAggiuntaAppartenenteInterceptor.java
@@ -50,41 +50,34 @@ public class FonteAggiuntaAppartenenteInterceptor extends RibaltoneBaseIntercept
         QStruttura qStruttura = QStruttura.struttura;
         fonteAggiuntaAppartenente.setCodiceMatricola(fonteAggiuntaAppartenente.getCodiceMatricola());
         Struttura struttura = queryFactory
-            .select(qStruttura)
-            .from(qStruttura)
-            .where(
-                qStruttura.attiva.and(
-                    qStruttura.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())
-                ).and(
-                    qStruttura.idAzienda.id.eq(fonteAggiuntaAppartenente.getIdAzienda())
-                )).fetchOne();
-        //vuol dire che sto aggiungendo un utente alla struttura destinazione di replica
-        if (struttura == null) {
-            struttura = queryFactory
                 .select(qStruttura)
                 .from(qStruttura)
                 .where(
-                    qStruttura.attiva.and(qStruttura.idStrutturaReplicata.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella()))).fetchOne();
-        }
+                        qStruttura.attiva.and(
+                                qStruttura.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())
+                        ).and(
+                                qStruttura.idAzienda.id.eq(fonteAggiuntaAppartenente.getIdAzienda())
+                        )).fetchOne();
+
         if (struttura != null) {
             DatiImportatiAppartenente datiImportatiAppartenente = queryFactory
-                .select(qDatiImportatiAppartenente)
-                .from(qDatiImportatiAppartenente)
-                .where(
-                    qDatiImportatiAppartenente.codiceFiscale.eq(fonteAggiuntaAppartenente.getCodiceFiscale())
-                        .and(qDatiImportatiAppartenente.idAzienda.eq(struttura.getIdAzienda().getId()))
-                )
-                .limit(1)
-                .fetchOne();
-            if (datiImportatiAppartenente == null) {
-                datiImportatiAppartenente = queryFactory
                     .select(qDatiImportatiAppartenente)
                     .from(qDatiImportatiAppartenente)
                     .where(
-                        qDatiImportatiAppartenente.codiceFiscale.eq(fonteAggiuntaAppartenente.getCodiceFiscale())
+                            qDatiImportatiAppartenente.codiceFiscale.eq(fonteAggiuntaAppartenente.getCodiceFiscale())
+                                    .and(qDatiImportatiAppartenente.idAzienda.eq(struttura.getIdAzienda().getId()))
                     )
                     .limit(1)
                     .fetchOne();
+            if (datiImportatiAppartenente == null) {
+                datiImportatiAppartenente = queryFactory
+                        .select(qDatiImportatiAppartenente)
+                        .from(qDatiImportatiAppartenente)
+                        .where(
+                                qDatiImportatiAppartenente.codiceFiscale.eq(fonteAggiuntaAppartenente.getCodiceFiscale())
+                        )
+                        .limit(1)
+                        .fetchOne();
             }
             fonteAggiuntaAppartenente.setCodiceMatricola(datiImportatiAppartenente != null ? datiImportatiAppartenente.getCodiceMatricola() : null);
             fonteAggiuntaAppartenente.setCodiceAzienda(struttura.getIdAzienda().getCodice());
@@ -102,40 +95,43 @@ public class FonteAggiuntaAppartenenteInterceptor extends RibaltoneBaseIntercept
         DatiImportatiAppartenente datoImportatoAppartenente = fonteAggiuntaAppartenente.buildDatoImportatoAppartenente();
         JPAQueryFactory queryFactory = new JPAQueryFactory(repositoryFactory.getEntityManager());
         Struttura struttura = queryFactory
-            .select(qStruttura)
-            .from(qStruttura)
-            .where(qStruttura.attiva.and(qStruttura.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella()))).limit(1)
-            .fetchOne();
+                .select(qStruttura)
+                .from(qStruttura)
+                .where(qStruttura.attiva.and(qStruttura.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella()))
+                        .and(qStruttura.idAzienda.id.eq(fonteAggiuntaAppartenente.getIdAzienda())))
+                .fetchOne();
+        //vuol dire che sto aggiungendo un utente alla struttura sorgente di replica
+
         OperationsUtils.insertUtenteInStruttura(queryFactory, fonteAggiuntaAppartenente.buildDatidaImportare(), struttura, repositoryFactory.getEntityManager(), repositoryFactory.getPermissionManager(), null, fonteAggiuntaAppartenente.getId());
 
         //gestito le repliche
         List<Struttura> struttureReplicate = queryFactory
-            .select(qStruttura)
-            .from(qStruttura)
-            .where(qStruttura.attiva.and(qStruttura.idStrutturaReplicata.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella()))).limit(1)
-            .fetch();
+                .select(qStruttura)
+                .from(qStruttura)
+                .where(qStruttura.attiva.and(qStruttura.idStrutturaReplicata.id.eq(struttura.getId()))).limit(1)
+                .fetch();
         if (struttureReplicate != null) {
             for (Struttura strutturaReplica : struttureReplicate) {
                 OperationsUtils.insertUtenteInStruttura(
-                    queryFactory,
-                    fonteAggiuntaAppartenente.buildDatidaImportare(),
-                    strutturaReplica,
-                    repositoryFactory.getEntityManager(),
-                    repositoryFactory.getPermissionManager(),
-                    null);
+                        queryFactory,
+                        fonteAggiuntaAppartenente.buildDatidaImportare(),
+                        strutturaReplica,
+                        repositoryFactory.getEntityManager(),
+                        repositoryFactory.getPermissionManager(),
+                        null);
             }
         }
         // gestisco le fusioni
         List<StrutturaUnificata> struttureUnificate = queryFactory
-            .select(qStrutturaUnificata)
-            .from(qStrutturaUnificata)
-            .where(
-                (qStrutturaUnificata.idStrutturaSorgente.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())
-                    .or(qStrutturaUnificata.idStrutturaDestinazione.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())))
-                    .and(qStrutturaUnificata.dataAccensioneAttivazione.before(ZonedDateTime.now()))
-                    .and(qStrutturaUnificata.dataDisattivazione.isNull())
-                    .and(qStrutturaUnificata.tipoOperazione.eq(StrutturaUnificata.TipoUnificazione.FUSIONE)))
-            .fetch();
+                .select(qStrutturaUnificata)
+                .from(qStrutturaUnificata)
+                .where(
+                        (qStrutturaUnificata.idStrutturaSorgente.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())
+                                .or(qStrutturaUnificata.idStrutturaDestinazione.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())))
+                                .and(qStrutturaUnificata.dataAccensioneAttivazione.before(ZonedDateTime.now()))
+                                .and(qStrutturaUnificata.dataDisattivazione.isNull())
+                                .and(qStrutturaUnificata.tipoOperazione.eq(StrutturaUnificata.TipoUnificazione.FUSIONE)))
+                .fetch();
         for (StrutturaUnificata strutturaUnificata : struttureUnificate) {
             Struttura strutturaDoveInserire = null;
             if (strutturaUnificata.getIdStrutturaDestinazione().getIdCasella().equals(fonteAggiuntaAppartenente.getIdCasella())) {
@@ -146,12 +142,12 @@ public class FonteAggiuntaAppartenenteInterceptor extends RibaltoneBaseIntercept
                 strutturaDoveInserire = strutturaUnificata.getIdStrutturaDestinazione();
             }
             OperationsUtils.insertUtenteInStruttura(
-                queryFactory,
-                fonteAggiuntaAppartenente.buildDatidaImportare(),
-                strutturaDoveInserire,
-                repositoryFactory.getEntityManager(),
-                repositoryFactory.getPermissionManager(),
-                null);
+                    queryFactory,
+                    fonteAggiuntaAppartenente.buildDatidaImportare(),
+                    strutturaDoveInserire,
+                    repositoryFactory.getEntityManager(),
+                    repositoryFactory.getPermissionManager(),
+                    null);
         }
         repositoryFactory.getEntityManager().persist(datoImportatoAppartenente);
 
@@ -230,42 +226,42 @@ public class FonteAggiuntaAppartenenteInterceptor extends RibaltoneBaseIntercept
         FonteAggiuntaAppartenente fonteAggiuntaAppartenente = (FonteAggiuntaAppartenente) entity;
         JPAQueryFactory queryFactory = new JPAQueryFactory(repositoryFactory.getEntityManager());
         Struttura struttura = queryFactory
-            .select(qStruttura)
-            .from(qStruttura)
-            .where(qStruttura.attiva.and(qStruttura.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella()))).limit(1)
-            .fetchOne();
+                .select(qStruttura)
+                .from(qStruttura)
+                .where(qStruttura.attiva.and(qStruttura.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella()))).limit(1)
+                .fetchOne();
         //da ragionare bene su utenti unificati
         OperationsUtils.chiudiUtenteStruttura(fonteAggiuntaAppartenente.buildDatidaImportare().buildDatiImportati(), struttura, fonteAggiuntaAppartenente.getIdAzienda(), queryFactory, repositoryFactory.getPermissionManager(), repositoryFactory.getEntityManager(), null);
 
         //gestito le repliche
         List<Struttura> struttureReplicate = queryFactory
-            .select(qStruttura)
-            .from(qStruttura)
-            .where(qStruttura.attiva.and(qStruttura.idStrutturaReplicata.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella()))).limit(1)
-            .fetch();
+                .select(qStruttura)
+                .from(qStruttura)
+                .where(qStruttura.attiva.and(qStruttura.idStrutturaReplicata.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella()))).limit(1)
+                .fetch();
         if (struttureReplicate != null) {
             for (Struttura strutturaReplica : struttureReplicate) {
                 OperationsUtils.chiudiUtenteStruttura(
-                    fonteAggiuntaAppartenente.buildDatidaImportare().buildDatiImportati(),
-                    strutturaReplica,
-                    strutturaReplica.getIdAzienda().getId(),
-                    queryFactory,
-                    repositoryFactory.getPermissionManager(),
-                    repositoryFactory.getEntityManager(),
-                    null);
+                        fonteAggiuntaAppartenente.buildDatidaImportare().buildDatiImportati(),
+                        strutturaReplica,
+                        strutturaReplica.getIdAzienda().getId(),
+                        queryFactory,
+                        repositoryFactory.getPermissionManager(),
+                        repositoryFactory.getEntityManager(),
+                        null);
             }
         }
         // gestisco le fusioni
         List<StrutturaUnificata> struttureUnificate = queryFactory
-            .select(qStrutturaUnificata)
-            .from(qStrutturaUnificata)
-            .where(
-                (qStrutturaUnificata.idStrutturaSorgente.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())
-                    .or(qStrutturaUnificata.idStrutturaDestinazione.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())))
-                    .and(qStrutturaUnificata.dataAccensioneAttivazione.before(ZonedDateTime.now()))
-                    .and(qStrutturaUnificata.dataDisattivazione.isNull())
-                    .and(qStrutturaUnificata.tipoOperazione.eq(StrutturaUnificata.TipoUnificazione.FUSIONE)))
-            .fetch();
+                .select(qStrutturaUnificata)
+                .from(qStrutturaUnificata)
+                .where(
+                        (qStrutturaUnificata.idStrutturaSorgente.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())
+                                .or(qStrutturaUnificata.idStrutturaDestinazione.idCasella.eq(fonteAggiuntaAppartenente.getIdCasella())))
+                                .and(qStrutturaUnificata.dataAccensioneAttivazione.before(ZonedDateTime.now()))
+                                .and(qStrutturaUnificata.dataDisattivazione.isNull())
+                                .and(qStrutturaUnificata.tipoOperazione.eq(StrutturaUnificata.TipoUnificazione.FUSIONE)))
+                .fetch();
         for (StrutturaUnificata strutturaUnificata : struttureUnificate) {
             Struttura strutturaDoveRimuovere = null;
             if (strutturaUnificata.getIdStrutturaDestinazione().getIdCasella().equals(fonteAggiuntaAppartenente.getIdCasella())) {
@@ -276,13 +272,13 @@ public class FonteAggiuntaAppartenenteInterceptor extends RibaltoneBaseIntercept
                 strutturaDoveRimuovere = strutturaUnificata.getIdStrutturaDestinazione();
             }
             OperationsUtils.chiudiUtenteStruttura(
-                fonteAggiuntaAppartenente.buildDatidaImportare().buildDatiImportati(),
-                strutturaDoveRimuovere,
-                strutturaDoveRimuovere.getIdAzienda().getId(),
-                queryFactory,
-                repositoryFactory.getPermissionManager(),
-                repositoryFactory.getEntityManager(),
-                null);
+                    fonteAggiuntaAppartenente.buildDatidaImportare().buildDatiImportati(),
+                    strutturaDoveRimuovere,
+                    strutturaDoveRimuovere.getIdAzienda().getId(),
+                    queryFactory,
+                    repositoryFactory.getPermissionManager(),
+                    repositoryFactory.getEntityManager(),
+                    null);
         }
         return entity;
     }
