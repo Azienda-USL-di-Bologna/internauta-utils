85174cc0f4b1d4c482b6cad72e5609a37b1e6532
QB <m.venturi@dilaxia.com>
2026-05-08 11:41:40 +0200

    RM-78901 aggiunto parametro DSC

--- parameters-manager/src/main/java/it/bologna/ausl/internauta/utils/parameters/manager/ParametriAziendeReader.java
+++ parameters-manager/src/main/java/it/bologna/ausl/internauta/utils/parameters/manager/ParametriAziendeReader.java
@@ -66,7 +66,8 @@ public class ParametriAziendeReader {
         pdfConverterUrl,
         abilitaFlussiInternauta,
         giorniPESenzaFascicolazioneSollecito,
-        parametriDirettoriDelibera
+        parametriDirettoriDelibera,
+        DSC
     }
 
     @Autowired
